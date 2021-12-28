ecc8c6360020421b078dd3a3d309930bca4a0c3a
Jason Knoll <jason.p.knoll@gmail.com>
2021-12-27 17:45:50 -0700

    Add !infectme command

--- main.py
+++ main.py
@@ -156,6 +156,24 @@ async def on_message(message):
     if 'damn daniel' in message.content.lower():
         await message.channel.send(f'https://c.tenor.com/sxLBjystCmIAAAAC/damn-daniel-one-piece.gif')
 
+    if message.content.lower().startswith('!infectme'):
+        x = int(message.content.lower().split()[1])
+        if x > 0:
+            y = 0
+            if message.author.nick == None:
+                name = message.author.name
+            else:
+                name = message.author.nick
+            if (len(name) + x) <= 32:
+                pass
+            else:
+                x = 32 - len(name)
+            if message.author.nick == None:
+                await message.author.edit(nick=f"{message.author.name}"+f"{emoji*x}")
+            else:
+                await message.author.edit(nick=f"{message.author.nick}"+f"{emoji*x}")
+
+            await message.channel.send(f'{message.author.nick}'.replace(f"{emoji}","")+f' has gained +{x} {emoji}')
 
     if 'ping' in message.content.lower():
         await message.channel.send('pong')
