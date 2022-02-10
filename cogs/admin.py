b8eb4478426bb97102b341562e6d7f9ba93f8d55
Jason Knoll <jason.p.knoll@gmail.com>
2022-02-09 18:08:06 -0700

    Add terminal login command to admin cog

--- cogs/admin.py
+++ cogs/admin.py
@@ -6,6 +6,12 @@ class Admin(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    # Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('We have logged in as {0.user}'
+        .format(self.client))
+
     # Before Commands
     async def cog_check(self, ctx):
         return ctx.author.guild_permissions.administrator
