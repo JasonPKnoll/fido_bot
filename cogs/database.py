b2bfc134fc0373c232ebffade24842389691cb7a
Jason Knoll <jason.p.knoll@gmail.com>
2022-03-03 21:39:16 -0700

    Add database methods for creating new guild settings

--- cogs/database.py
+++ cogs/database.py
@@ -11,5 +11,30 @@ class Database(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    async def add_guild(self, id):
+        try:
+            self.client.db_guilds.find({'_id': id})[0]
+        except IndexError:
+            data = {
+            '_id': guild.id,
+            'emoji': '🌽',
+            'designated_channel': None,
+            'adder_word': 'this',
+            'subtractor_word': 'that',
+            'lottery_word': 'spread'
+            }
+
+            self.client.db_guilds.insert_one(data);
+
+    # Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        for guild in self.client.guilds:
+            await self.add_guild(guild.id)
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        await self.add_guild(guild.id)
+
 def setup(client):
     client.add_cog(Database(client))
